b2b7eeb2992174016bfcf7848909576dfc8626ea
Amanda Vencimento <amandavencimento@gmail.com>
2021-02-25 00:41:34 -0300

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/global.css'
+
+{/* Tudo que for de uso completo da aplicação será posto aqui */}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
